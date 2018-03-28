79d368b96c580d0cad0414b1f0697ea07e146e47
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-03-28 17:43:16 +0700

    update sudah di associate tag dengan project

--- models/project.js
+++ models/project.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Project = sequelize.define('Project', {
+    TagId: DataTypes.INTEGER,
     title: DataTypes.STRING,
     status: DataTypes.STRING,
     location: DataTypes.STRING,
@@ -11,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     summary: DataTypes.STRING
   }, {});
   Project.associate = function(models) {
+    Project.belongsTo(models.Tag)
     // associations can be defined here
   };
   return Project;
--- models/tag.js
+++ models/tag.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING
   }, {});
   Tag.associate = function(models) {
+    Tag.hasMany(models.Project)
     // associations can be defined here
   };
   return Tag;

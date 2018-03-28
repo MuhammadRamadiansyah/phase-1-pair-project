caa1d22d53d1e7a80876434722d9c80afc8781cb
rg872 <gilang0293@gmail.com>
2018-03-28 21:48:42 +0700

    home yang fitur search dan link belum di implement

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('sequelize');
+
+const session = require('express-session')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const path = require('path');
+const Op = sequelize.Op;
+const db = require('./models');
+const app = express();
+
+//buat setting path resources external (contohnya img)
+process.env.PWD = process.cwd()
+app.use(express.static(process.env.PWD + '/'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.listen(3000, ()=>{
+    console.log('server started in port 3000')
+});
